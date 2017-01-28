b2be0f2e6c17576920ba05f00c933caa666f608d
Rob Blake <robertb@property24.com>
2017-01-28 07:47:42 +0200

    Added unit tests for DataSources.

---
+++ Code/UnitTests/Infrastructure.DataSources.UnitTests/ExcelDataSourceTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DataSources.UnitTests
+{
+    [TestFixture]
+    public class ExcelDataSourceTests
+    {
+        [Test]
+        public void TestItWorks()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
